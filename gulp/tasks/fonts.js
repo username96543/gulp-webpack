911ba0ce61cf8009c60b5b9e63bd3bb4065ca127
username96543 <114392005+username96543@users.noreply.github.com>
2023-05-27 19:49:42 +0700

    Update fonts.js

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -48,77 +48,88 @@ export const ttfToWoff = () => {
   );
 };
 
-export const fontsStyle = () => {
+export const woffDest = () => {
+  return app.gulp
+    .src(`${app.path.srcFolder}/fonts/*.woff2`, {})
+    .pipe(app.gulp.dest(`${app.path.build.fonts}`));
+};
+
+export const fontsBase64 = () => {
   // Файл стилей подключения шрифтов
   let fontsFile = `${app.path.srcFolder}/sass/fonts.scss`;
   // Проверяем существуют ли файлы шрифтов
+
   fs.readdir(app.path.build.fonts, function (err, fontsFiles) {
     if (fontsFiles) {
-      // Проверяем существует ли файл стилей для подключения шрифтов
-      if (!fs.existsSync(fontsFile)) {
-        // Если файла нет, создаем его
-        fs.writeFile(fontsFile, "", cb);
-        let newFileOnly;
-        for (let i = 0; i < fontsFiles.length; i++) {
-          let fontBase64 = fs.readFileSync(
-            app.path.build.fonts + fontsFiles[i],
-            {
-              encoding: "base64",
-            }
-          );
-          // Записываем подключения шрифтов в файл стилей
-          let fontFileName = fontsFiles[i].split(".")[0];
-          if (newFileOnly !== fontFileName) {
-            let fontName = fontFileName.split("-")[0]
-              ? fontFileName.split("-")[0]
-              : fontFileName;
-            let fontWeight = fontFileName.split("-")[1]
-              ? fontFileName.split("-")[1]
-              : fontFileName;
-            if (fontWeight.toLowerCase() === "thin") {
-              fontWeight = 100;
-            } else if (fontWeight.toLowerCase() === "extralight") {
-              fontWeight = 200;
-            } else if (fontWeight.toLowerCase() === "light") {
-              fontWeight = 300;
-            } else if (fontWeight.toLowerCase() === "medium") {
-              fontWeight = 500;
-            } else if (fontWeight.toLowerCase() === "semibold") {
-              fontWeight = 600;
-            } else if (fontWeight.toLowerCase() === "bold") {
-              fontWeight = 700;
-            } else if (
-              fontWeight.toLowerCase() === "extrabold" ||
-              fontWeight.toLowerCase() === "heavy"
-            ) {
-              fontWeight = 800;
-            } else if (fontWeight.toLowerCase() === "black") {
-              fontWeight = 900;
-            } else {
-              fontWeight = 400;
-            }
-            fs.appendFile(
-              //src: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");
-              fontsFile,
-              `@font-face {
+      if (err) {
+        console.log(err);
+      } else {
+        // Проверяем существует ли файл стилей для подключения шрифтов
+        if (!fs.existsSync(fontsFile)) {
+          // Если файла нет, создаем его
+          fs.writeFile(fontsFile, "", cb);
+          let newFileOnly;
+          for (let i = 0; i < fontsFiles.length; i++) {
+            let fontBase64 = fs.readFileSync(
+              app.path.build.fonts + fontsFiles[i],
+              {
+                encoding: "base64",
+              }
+            );
+            // Записываем подключения шрифтов в файл стилей
+            let fontFileName = fontsFiles[i].split(".")[0];
+            if (newFileOnly !== fontFileName) {
+              let fontName = fontFileName.split("-")[0]
+                ? fontFileName.split("-")[0]
+                : fontFileName;
+              let fontWeight = fontFileName.split("-")[1]
+                ? fontFileName.split("-")[1]
+                : fontFileName;
+              if (fontWeight.toLowerCase() === "thin") {
+                fontWeight = 100;
+              } else if (fontWeight.toLowerCase() === "extralight") {
+                fontWeight = 200;
+              } else if (fontWeight.toLowerCase() === "light") {
+                fontWeight = 300;
+              } else if (fontWeight.toLowerCase() === "medium") {
+                fontWeight = 500;
+              } else if (fontWeight.toLowerCase() === "semibold") {
+                fontWeight = 600;
+              } else if (fontWeight.toLowerCase() === "bold") {
+                fontWeight = 700;
+              } else if (
+                fontWeight.toLowerCase() === "extrabold" ||
+                fontWeight.toLowerCase() === "heavy"
+              ) {
+                fontWeight = 800;
+              } else if (fontWeight.toLowerCase() === "black") {
+                fontWeight = 900;
+              } else {
+                fontWeight = 400;
+              }
+              fs.appendFile(
+                //src: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");
+                fontsFile,
+                `@font-face {
                 font-family: ${fontName};
                 font-display: swap;
                 src: url(data:application/x-font-woff;charset=utf-8;base64,${fontBase64}) format("woff2");
                 font-weight: ${fontWeight};
                 font-style: normal;
               }\r\n`,
-              cb
-            );
-            // `@font-face {\n\tfont-family: ${fontName};\n\tfont-display: swap;}
-            newFileOnly = fontFileName;
+                cb
+              );
+              // `@font-face {\n\tfont-family: ${fontName};\n\tfont-display: swap;}
+              newFileOnly = fontFileName;
+            }
           }
+        } else {
+          // Если файл есть, то выводим сообщение
+          console.log(
+            "Файл scss/fonts.scss уже существует. Для обновления нужно его удалить.",
+            "Это нужно для того, чтобы можно было дописывать свои изменения в файл."
+          );
         }
-      } else {
-        // Если файл есть, то выводим сообщение
-        console.log(
-          "Файл scss/fonts.scss уже существует. Для обновления нужно его удалить.",
-          "Это нужно для того, чтобы можно было дописывать свои изменения в файл."
-        );
       }
     }
   });
