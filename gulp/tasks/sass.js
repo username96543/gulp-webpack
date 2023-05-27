fb22c46f53674ee65e36ae64b1aa05b9d0c49bdb
username96543 <114392005+username96543@users.noreply.github.com>
2023-05-27 19:51:57 +0700

    Update sass.js

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,41 +1,52 @@
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-import rename from 'gulp-rename';
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import rename from "gulp-rename";
 
-import cleanCss from 'gulp-clean-css';
-import webpCss from 'gulp-webpcss';
-import autoprefixer from 'gulp-autoprefixer';
-import groupCssMediaQueries from 'gulp-group-css-media-queries';
+import cleanCss from "gulp-clean-css";
+//import webpCss from 'gulp-webpcss';
+import autoprefixer from "gulp-autoprefixer";
+import groupCssMediaQueries from "gulp-group-css-media-queries";
 
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
-  return app.gulp.src(app.path.src.sass, { sourcemaps: true })
-    .pipe(app.plugins.plumber(
-      app.plugins.notify.onError({
-        title: 'SCSS',
-        message: 'Error: <%= error.message %>'
-      })
-    ))
-    .pipe(app.plugins.replace(/@img\//g, '../img/'))
-    .pipe(sass({
-      output: 'expanded'
-    }))
-    .pipe(groupCssMediaQueries())
-    .pipe(webpCss({
-      webpClass: '.webp',
-      noWebpClass: '.no-webp'
-    }))
-    .pipe(autoprefixer({
-      grid: true,
-      overrideBrowserlist: ["last 3 versions"],
-      cascade: true
-    }))
-    .pipe(app.gulp.dest(app.path.build.css))
-    .pipe(cleanCss())
-    .pipe(app.plugins.browsersync.stream())
-    .pipe(rename({
-      extname: '.min.css'
-    }))
-    .pipe(app.gulp.dest(app.path.build.css))
-}
+  return (
+    app.gulp
+      .src(app.path.src.sass, { sourcemaps: true })
+      .pipe(
+        app.plugins.plumber(
+          app.plugins.notify.onError({
+            title: "SCSS",
+            message: "Error: <%= error.message %>",
+          })
+        )
+      )
+      .pipe(app.plugins.replace(/@img\//g, "../img/"))
+      .pipe(
+        sass({
+          output: "expanded",
+        })
+      )
+      .pipe(groupCssMediaQueries())
+      // .pipe(webpCss({
+      //   webpClass: '.webp',
+      //   noWebpClass: '.no-webp'
+      // }))
+      .pipe(
+        autoprefixer({
+          grid: true,
+          overrideBrowserlist: ["last 3 versions"],
+          cascade: true,
+        })
+      )
+      .pipe(app.gulp.dest(app.path.build.css))
+      .pipe(cleanCss())
+      .pipe(app.plugins.browsersync.stream())
+      .pipe(
+        rename({
+          extname: ".min.css",
+        })
+      )
+      .pipe(app.gulp.dest(app.path.build.css))
+  );
+};
