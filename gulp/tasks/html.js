9be20e05bb019df518d190d7d5d128327d651bb3
username96543 <114392005+username96543@users.noreply.github.com>
2023-05-27 19:38:13 +0700

    Update html.js

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,13 +1,25 @@
 import fileInclude from "gulp-file-include";
-import webpHTML from "gulp-webp-html-nosvg";
+//import webpHTML from "gulp-webp-html-nosvg";
 //import webpHTML from 'gulp-webp-html';
 import versionNumber from "gulp-version-number";
 //import pug from "gulp-pug";
 
-export const html = () => {
+export const html = (file) => {
+  let path = app.path.src.html;
+  if (typeof file !== "function") {
+    path = `./${file}`;
+
+    let file1 = {};
+    file1.path = path;
+    file1.arr = path.split("/");
+    file1.name = file1.arr[file1.arr.length - 1];
+
+    path = `./src/wordpress/**/${file1.name}`;
+  }
+
   return (
     app.gulp
-      .src(app.path.src.html)
+      .src(path)
       .pipe(
         app.plugins.plumber(
           app.plugins.notify.onError({
@@ -23,10 +35,25 @@ export const html = () => {
       //   // Показывать в терминале какой файл обработан
       //   verbose: true
       // }))
-      .pipe(app.plugins.replace(/@css\//g, "./assets/css/"))
-      .pipe(app.plugins.replace(/@js\//g, "./assets/js/"))
-      .pipe(app.plugins.replace(/@img\//g, "./assets/img/"))
-      .pipe(webpHTML())
+      .pipe(
+        app.plugins.replace(
+          /@css\//g,
+          "<?php echo get_template_directory_uri(); ?>/assets/css/"
+        )
+      )
+      .pipe(
+        app.plugins.replace(
+          /@js\//g,
+          "<?php echo get_template_directory_uri(); ?>/assets/js/"
+        )
+      )
+      .pipe(
+        app.plugins.replace(
+          /@img\//g,
+          "<?php echo get_template_directory_uri(); ?>/assets/img/"
+        )
+      )
+      //.pipe(webpHTML())
       .pipe(
         versionNumber({
           value: "%DT%",
